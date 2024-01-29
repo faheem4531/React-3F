b917d62f994b683b47fa0c4ea55d5ab8e7d916a7
faheem4531 <faheem4531@gmail.com>
2024-01-30 01:28:20 +0500

    optimizaition via useMemo

--- lessons/42 First R3F Application/src/CustomObject.jsx
+++ lessons/42 First R3F Application/src/CustomObject.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useMemo } from "react";
 import * as THREE from 'three'
 
 const CustomObject = () => {
 
-  const verticesCount = 10 * 3
-  const positions = new Float32Array(verticesCount * 3)
+  const verticesCount = 30
 
 
-  for (let i = 0; i < verticesCount * 3; i++) {
-    positions[i] = (Math.random() - 0.5) * 3
 
-  }
+  const positions = useMemo(() => {
 
+    const positions = new Float32Array(verticesCount * 3)
+
+
+    for (let i = 0; i < verticesCount * 3; i++)
+      positions[i] = (Math.random() - 0.5) * 3
+
+    return positions
+  }, [])
 
   return (
     <mesh>
