4346d1781533f13064aed5f161c3bafbc4207cc5
faheem4531 <faheem4531@gmail.com>
2024-01-29 15:58:30 +0500

    first basic gemoetry view

--- lessons/42 First R3F Application/src/index.jsx
+++ lessons/42 First R3F Application/src/index.jsx
@@ -1,10 +1,12 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
+import App from './App'
+
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <>
-        Soon to be a badass R3F application
+        <App />
     </>
 )
