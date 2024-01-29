2fcf3b24747fe386e12073972d9063baa103f986
faheem4531 <faheem4531@gmail.com>
2024-01-29 23:39:58 +0500

    group animatimon

--- lessons/42 First R3F Application/src/Experience.jsx
+++ lessons/42 First R3F Application/src/Experience.jsx
@@ -4,24 +4,28 @@ import React, { useRef } from "react";
 const Experience = () => {
 
   const cubeRef = useRef()
+  const groupRef = useRef()
 
   //useFrame hook will be called on each frame ==> to animate etc...
   useFrame((state, delta) => {
     cubeRef.current.rotation.y += delta
+    groupRef.current.rotation.y += delta
   })
 
   return (
 
     <>
-      <mesh position-x={-2} scale={0.8}>
-        <sphereGeometry />
-        <meshBasicMaterial color="orange" wireframe />
-      </mesh>
+      <group ref={groupRef}>
+        <mesh position-x={-2} scale={0.8}>
+          <sphereGeometry />
+          <meshBasicMaterial color="orange" wireframe />
+        </mesh>
 
-      <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
-        <boxGeometry scale={1.5} />
-        <meshBasicMaterial color="mediumpurple" wireframe={false} />
-      </mesh>
+        <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+          <boxGeometry scale={1.5} />
+          <meshBasicMaterial color="mediumpurple" wireframe={false} />
+        </mesh>
+      </group>
 
       <mesh position-y={-1} rotation-x={- Math.PI * 0.5} scale={10}>
         <planeGeometry />
