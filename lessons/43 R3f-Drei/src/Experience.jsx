bc9d69f5f091e34874ed1a00aa5d813b2273a7eb
faheem4531 <faheem4531@gmail.com>
2024-01-30 17:28:25 +0500

    TransformControls => single obj  controls

--- lessons/43 R3f-Drei/src/Experience.jsx
+++ lessons/43 R3f-Drei/src/Experience.jsx
@@ -1,9 +1,12 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, TransformControls } from "@react-three/drei";
+import { useRef } from "react";
 
 export default function Experience() {
 
+    const cubeRef = useRef()
+
     return <>
-        <OrbitControls />
+        <OrbitControls makeDefault />
 
         <directionalLight position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} />
@@ -13,10 +16,12 @@ export default function Experience() {
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh position-x={2} scale={1.5}>
+        <mesh position-x={2} scale={1.5} ref={cubeRef}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
+        <TransformControls object={cubeRef} mode="translate" /> {/* mode = rotate,scale */}
+
 
         <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
