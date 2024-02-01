cfd3a624124f8ce73ef00fee6d52a2117ce25e5b
faheem4531 <faheem4531@gmail.com>
2024-02-02 03:19:34 +0500

    model componenet

--- lessons/46 Load Models with R3F/src/Experience.jsx
+++ lessons/46 Load Models with R3F/src/Experience.jsx
@@ -1,23 +1,8 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import Model from './Model'
 
 export default function Experience() {
-
-    // const model = useLoader(GLTFLoader, './hamburger.glb')
-
-    const model = useLoader(
-        GLTFLoader,
-        './hamburger-draco.glb',
-        (loader) => {
-            const dracoLoader = new DRACOLoader()
-            dracoLoader.setDecoderPath('./draco/')
-            loader.setDRACOLoader(dracoLoader)
-        }
-    )
-
 
     return <>
 
@@ -28,14 +13,10 @@ export default function Experience() {
         <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} />
 
-
-
         <mesh receiveShadow position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
-
-        <primitive object={model.scene} scale={0.35} />
-
+        <Model />
     </>
 }
