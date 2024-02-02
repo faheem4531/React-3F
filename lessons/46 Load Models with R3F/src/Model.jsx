15d2a7396c455d2316322301455bdb8ca403bd83
faheem4531 <faheem4531@gmail.com>
2024-02-03 01:58:00 +0500

    load model with drie

--- lessons/46 Load Models with R3F/src/Model.jsx
+++ lessons/46 Load Models with R3F/src/Model.jsx
@@ -1,21 +1,24 @@
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { useGLTF } from '@react-three/drei'
 
 
 export default function Model() {
   // const model = useLoader(GLTFLoader, './hamburger.glb')
 
-  const model = useLoader(
-    GLTFLoader,
-    './FlightHelmet/glTF/FlightHelmet.gltf',
-    (loader) => {
-      const dracoLoader = new DRACOLoader()
-      dracoLoader.setDecoderPath('./draco/')
-      loader.setDRACOLoader(dracoLoader)
-    }
+  // const model = useLoader(
+  //   GLTFLoader,
+  //   './FlightHelmet/glTF/FlightHelmet.gltf',
+  //   (loader) => {
+  //     const dracoLoader = new DRACOLoader()
+  //     dracoLoader.setDecoderPath('./draco/')
+  //     loader.setDRACOLoader(dracoLoader)
+  //   }
+  // )
+
+
+  const model = useGLTF('./hamburger.glb'
   )
-
-
-  return <primitive object={model.scene} scale={5} position-y={-1} />
+  return <primitive object={model.scene} scale={0.35} />
 }
