932465c7fd4d43cc0c22a28a91f61bc5a6c40273
faheem4531 <faheem4531@gmail.com>
2024-02-03 17:33:49 +0500

    fox leva movement animation

--- lessons/46 Load Models with R3F/src/Fox.jsx
+++ lessons/46 Load Models with R3F/src/Fox.jsx
@@ -1,20 +1,23 @@
 import { useAnimations, useGLTF } from '@react-three/drei'
+import { useControls } from 'leva'
 import { useEffect } from 'react'
 
 export default function Fox() {
   const fox = useGLTF('./Fox/glTF/Fox.gltf')
   const animations = useAnimations(fox.animations, fox.scene)
 
+  const { animationName } = useControls({
+    animationName: { options: ['Survey', 'Walk', 'Run'] }
+  })
+
   useEffect(() => {
-    const action = animations.actions.Run
-    action.play()
+    const action = animations.actions[animationName]
+    action.reset().fadeIn(0.5).play()
 
-    setTimeout(() => {
-      animations.actions.Walk.play()
-      animations.actions.Walk.crossFadeFrom(animations.actions.Run, 1)
-
-    }, 2000)
-  }, [])
+    return () => {
+      action.fadeOut(0.5)
+    }
+  }, [animationName])
 
 
   return <primitive
