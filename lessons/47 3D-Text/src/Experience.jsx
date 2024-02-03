18f41e4dcd3b3c88045af5d0e455d1a4774529ef
faheem4531 <faheem4531@gmail.com>
2024-02-03 18:36:19 +0500

    load Text3D

--- lessons/47 3D-Text/src/Experience.jsx
+++ lessons/47 3D-Text/src/Experience.jsx
@@ -1,18 +1,29 @@
-import { OrbitControls } from '@react-three/drei'
+import { Center, OrbitControls, Text3D } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 
-export default function Experience()
-{
+export default function Experience() {
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <Center>
+            <Text3D
+                font="./fonts/helvetiker_regular.typeface.json"
+                size={0.75}
+                height={0.2}
+                curveSegments={12}
+                bevelEnabled
+                bevelThickness={0.02}
+                bevelSize={0.02}
+                bevelOffset={0}
+                bevelSegments={5}
+            >
+                HELLO R3F
+                <meshNormalMaterial />
+            </Text3D>
+        </Center>
 
     </>
 }
