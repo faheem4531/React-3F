3b93d68ee7108362afd00052ea60cb05f5c38e40
faheem4531 <faheem4531@gmail.com>
2024-02-03 16:55:49 +0500

    apply shadow

--- lessons/46 Load Models with R3F/src/Model.jsx
+++ lessons/46 Load Models with R3F/src/Model.jsx
@@ -21,9 +21,9 @@ export default function Model() {
   const model = useGLTF('./hamburger-draco.glb')
 
   return <>
-    <Clone object={model.scene} scale={0.35} position-x={4} />
-    <Clone object={model.scene} scale={0.35} position-x={-4} />
-    <Clone object={model.scene} scale={0.35} position-x={0} />
+    <Clone object={model.scene} scale={0.35} position-x={0} castShadow />
+    {/* <Clone object={model.scene} scale={0.35} position-x={-4} />
+    <Clone object={model.scene} scale={0.35} position-x={4} /> */}
   </>
 }
 
