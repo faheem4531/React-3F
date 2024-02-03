7949a24f3a9715880b4165e000ce85884881fdd0
faheem4531 <faheem4531@gmail.com>
2024-02-03 21:35:55 +0500

    Add poleLights and Portal Light

--- lessons/48 Portal Scene with R3F/src/Experience.jsx
+++ lessons/48 Portal Scene with R3F/src/Experience.jsx
@@ -15,5 +15,27 @@ export default function Experience() {
         <mesh geometry={nodes.baked.geometry} >
             <meshBasicMaterial map={bakedTexture} />
         </mesh>
+
+        <mesh
+            geometry={nodes.poleLightA.geometry}
+            position={nodes.poleLightA.position}
+        >
+            <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+
+        <mesh
+            geometry={nodes.poleLightB.geometry}
+            position={nodes.poleLightB.position}
+        >
+            <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+
+        <mesh
+            geometry={nodes.portalLight.geometry}
+            position={nodes.portalLight.position}
+            rotation={nodes.portalLight.rotation}
+
+        >
+        </mesh>
     </>
 }
