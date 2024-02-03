81963a6f503e6e25e979b1e5f300c43e46d53022
faheem4531 <faheem4531@gmail.com>
2024-02-03 19:11:21 +0500

    map torusGeometry at diff. positions

--- lessons/47 3D-Text/src/Experience.jsx
+++ lessons/47 3D-Text/src/Experience.jsx
@@ -6,6 +6,10 @@ export default function Experience() {
 
     // https://github.com/nidorx/matcaps/blob/master/PAGE-17.md
     const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+    const [matcapTextureX] = useMatcapTexture('7E5344_22120E_CCAB9C_8D919D', 256)
+
+
+    // const tempArray = [...Array[100]]
 
     return <>
 
@@ -29,6 +33,26 @@ export default function Experience() {
                 <meshMatcapMaterial matcap={matcapTexture} />
             </Text3D>
         </Center>
+
+        {[...Array(100)].map((value, index) =>
+            <mesh
+                key={index}
+                position={[
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10
+                ]}
+                scale={0.2 + Math.random() * 0.2}
+                rotation={[
+                    Math.random() * Math.PI,
+                    Math.random() * Math.PI,
+                    0
+                ]}
+            >
+                <torusGeometry args={[1, 0.6, 16, 32]} />
+                <meshMatcapMaterial matcap={matcapTextureX} />
+            </mesh>
+        )}
 
     </>
 }
