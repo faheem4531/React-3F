e84ebb6cdeb5703e3b65f19a30074836dd342149
faheem4531 <faheem4531@gmail.com>
2024-02-03 21:41:25 +0500

    Sparkles => for adding light flyes feature

--- lessons/48 Portal Scene with R3F/src/Experience.jsx
+++ lessons/48 Portal Scene with R3F/src/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import { Center, OrbitControls, Sparkles, useGLTF, useTexture } from '@react-three/drei'
 
 export default function Experience() {
 
@@ -12,30 +12,40 @@ export default function Experience() {
 
         <OrbitControls makeDefault />
 
-        <mesh geometry={nodes.baked.geometry} >
-            <meshBasicMaterial map={bakedTexture} />
-        </mesh>
+        <Center>
+            <mesh geometry={nodes.baked.geometry} >
+                <meshBasicMaterial map={bakedTexture} />
+            </mesh>
 
-        <mesh
-            geometry={nodes.poleLightA.geometry}
-            position={nodes.poleLightA.position}
-        >
-            <meshBasicMaterial color="#ffffe5" />
-        </mesh>
+            <mesh
+                geometry={nodes.poleLightA.geometry}
+                position={nodes.poleLightA.position}
+            >
+                <meshBasicMaterial color="#ffffe5" />
+            </mesh>
 
-        <mesh
-            geometry={nodes.poleLightB.geometry}
-            position={nodes.poleLightB.position}
-        >
-            <meshBasicMaterial color="#ffffe5" />
-        </mesh>
+            <mesh
+                geometry={nodes.poleLightB.geometry}
+                position={nodes.poleLightB.position}
+            >
+                <meshBasicMaterial color="#ffffe5" />
+            </mesh>
 
-        <mesh
-            geometry={nodes.portalLight.geometry}
-            position={nodes.portalLight.position}
-            rotation={nodes.portalLight.rotation}
+            <mesh
+                geometry={nodes.portalLight.geometry}
+                position={nodes.portalLight.position}
+                rotation={nodes.portalLight.rotation}
 
-        >
-        </mesh>
+            >
+            </mesh>
+            <Sparkles
+                size={6}
+                scale={[4, 2, 4]}
+                position-y={1}
+                speed={0.2}
+                count={40}
+            />
+        </Center>
+
     </>
 }
