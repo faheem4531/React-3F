33742b1eea7fd640941e4697965ce8c5229da94e
faheem4531 <faheem4531@gmail.com>
2024-02-04 23:17:52 +0500

    onClick color change and stopPropagation => not to click if sphere overrides cube

--- lessons/49 Mouse Events/src/Experience.jsx
+++ lessons/49 Mouse Events/src/Experience.jsx
@@ -2,33 +2,37 @@ import { useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
 
-export default function Experience()
-{
+export default function Experience() {
     const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
+
+    useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2
     })
 
+    // onClick / onContextMenu / onDoubleClick / onPointerUp => when we release the click or touch / onPointerDown / onPointerOver / onPointerEnter
+    // onPointerOut / onPointerLeave / onPointerMove / onPointerMissed 
+
+    const eventHandler = () => {
+        cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75% )`)
+    }
     return <>
 
         <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+        <directionalLight position={[1, 2, 3]} intensity={1.5} />
+        <ambientLight intensity={0.5} />
 
-        <mesh position-x={ - 2 }>
+        <mesh position-x={- 2} onClick={(event) => event.stopPropagation()}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={cube} position-x={2} scale={1.5} onClick={eventHandler}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+        <mesh position-y={- 1} rotation-x={- Math.PI * 0.5} scale={10}>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
