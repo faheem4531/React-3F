f2f8522ba76b989b3a12c69775dadd9752933a1a
faheem4531 <faheem4531@gmail.com>
2024-02-04 23:54:40 +0500

    load burger modal

--- lessons/49 Mouse Events/src/Experience.jsx
+++ lessons/49 Mouse Events/src/Experience.jsx
@@ -1,9 +1,10 @@
 import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF } from '@react-three/drei'
 import { useRef } from 'react'
 
 export default function Experience() {
     const cube = useRef()
+    const hamburger = useGLTF('./hamburger.glb')
 
     useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2
@@ -44,5 +45,14 @@ export default function Experience() {
             <meshStandardMaterial color="greenyellow" />
         </mesh>
 
+        <primitive
+            object={hamburger.scene}
+            scale={0.25}
+            position-y={0.5}
+            onClick={(event) => {
+                console.log(event.object.name);
+                event.stopPropagation()
+            }}
+        />
     </>
 }
