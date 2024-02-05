2689f386648bef2f61d06d411b5f9e07c09ffb19
faheem4531 <faheem4531@gmail.com>
2024-02-05 17:36:40 +0500

    RigidBody & Debug

--- lessons/52 Physics/src/Experience.jsx
+++ lessons/52 Physics/src/Experience.jsx
@@ -1,31 +1,39 @@
 import { OrbitControls } from '@react-three/drei'
+import { Debug, Physics, RigidBody } from '@react-three/rapier'
 import { Perf } from 'r3f-perf'
 
-export default function Experience()
-{
+export default function Experience() {
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+        <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+        <ambientLight intensity={0.5} />
 
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <Physics>
 
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+            <Debug />
+            <RigidBody>
+                <mesh castShadow position={[- 2, 2, 0]}>
+                    <sphereGeometry />
+                    <meshStandardMaterial color="orange" />
+                </mesh>
+            </RigidBody>
 
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+            <mesh castShadow position={[2, 2, 0]}>
+                <boxGeometry />
+                <meshStandardMaterial color="mediumpurple" />
+            </mesh>
+
+            <RigidBody type='fixed'>
+                <mesh receiveShadow position-y={- 1.25}>
+                    <boxGeometry args={[10, 0.5, 10]} />
+                    <meshStandardMaterial color="greenyellow" />
+                </mesh>
+            </RigidBody>
+        </Physics>
 
     </>
 }
