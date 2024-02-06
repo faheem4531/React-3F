ab2f2d5ee6b337b06b6700fc9187a1007a0b5d3a
faheem4531 <faheem4531@gmail.com>
2024-02-06 16:11:02 +0500

    gravity and objSectings

--- lessons/52 Physics/src/Experience.jsx
+++ lessons/52 Physics/src/Experience.jsx
@@ -1,8 +1,19 @@
 import { OrbitControls } from '@react-three/drei'
 import { Debug, Physics, RigidBody } from '@react-three/rapier'
 import { Perf } from 'r3f-perf'
+import { useRef } from 'react'
 
 export default function Experience() {
+    const cube = useRef()
+
+    const cubeJump = () => {
+        cube.current.applyImpulse({ x: 0, y: 5, z: 0 }) //apply force at y axis
+        cube.current.applyTorqueImpulse({
+            x: Math.random() - 0.5,
+            y: Math.random() - 0.5,
+            z: Math.random() - 0.5
+        })
+    }
     return <>
 
         <Perf position="top-left" />
@@ -12,19 +23,20 @@ export default function Experience() {
         <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
         <ambientLight intensity={0.5} />
 
-        <Physics>
+        <Physics gravity={[0, -9.81, 0]}>
 
             <Debug />
             {/* colliders = ball / trimesh / hull */}
-            <RigidBody colliders="ball">
-                <mesh castShadow position={[- 1.5, 2, 0]}>
+
+            <RigidBody colliders="ball" position={[- 1.5, 2, 0]}>
+                <mesh castShadow >
                     <sphereGeometry />
                     <meshStandardMaterial color="orange" />
                 </mesh>
             </RigidBody>
 
-            <RigidBody>
-                <mesh castShadow position={[1.5, 2, 0]}>
+            <RigidBody ref={cube} position={[1.5, 2, 0]} gravityScale={1}>
+                <mesh castShadow onClick={cubeJump}>
                     <boxGeometry />
                     <meshStandardMaterial color="mediumpurple" />
                 </mesh>
